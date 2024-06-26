702bcbf4b29509c95b8827ff02c991e580549a25
Desond Anane <doanane@st.ug.edu.gh>
2024-06-26 17:16:48 +0000

    transaction deal

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ThemeProvider, useTheme } from './ThemeContext';
+import { Home, MyCards, Statistics, Settings } from './screens';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <MainApp />
+    </ThemeProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MainApp = () => {
+  const { theme } = useTheme();
+
+  const screenOptions = {
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarStyle: {
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      left: 0,
+      elevation: 0,
+      height: 80,
+      width: '100%',
+      backgroundColor: theme.tabBar,
+    },
+  };
+
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                  source={require('./assets/home.png')}
+                  style={{ height: 30, width: 30, tintColor: focused ? theme.iconFocused : theme.icon }}
+                />
+                <Text style={{ fontSize: 12, color: focused ? theme.iconFocused : theme.text, marginBottom: 20 }}>
+                  Home
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="My Cards"
+          component={MyCards}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                  source={require('./assets/wallet.png')}
+                  style={{ height: 30, width: 30, tintColor: focused ? theme.iconFocused : theme.icon }}
+                />
+                <Text style={{ fontSize: 12, color: focused ? theme.iconFocused : theme.text, marginBottom: 20 }}>
+                  MyCards
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Statistics"
+          component={Statistics}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                  source={require('./assets/pie-chart.png')}
+                  style={{ height: 30, width: 30, tintColor: focused ? theme.iconFocused : theme.icon }}
+                />
+                <Text style={{ fontSize: 12, color: focused ? theme.iconFocused : theme.text, marginBottom: 20 }}>
+                  Statistics
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                  source={require('./assets/setting.png')}
+                  style={{ height: 30, width: 30, tintColor: focused ? theme.iconFocused : theme.icon }}
+                />
+                <Text style={{ fontSize: 12, color: focused ? theme.iconFocused : theme.text, marginBottom: 20 }}>
+                  Settings
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+     <StatusBar backgroundColor="#000" barStyle="light-content" />
+    </NavigationContainer>
+  );
+};
+
+export default App;
